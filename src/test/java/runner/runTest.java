9d393e48fb4e51d27b161e98ad2ddd620f1c36c9
aliboztemir <aliboztemir@gmail.com>
2021-03-03 03:31:50 +0300

    update parallel execution
    update parallel execution

--- src/test/java/runner/runTest.java
+++ src/test/java/runner/runTest.java
@@ -5,13 +5,12 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-        features = "src\\main\\resources\\features",
-        glue = "steps",
-        plugin = "pretty",
-        tags = "@Web"
-)
+@CucumberOptions(features = "src\\main\\resources\\features", glue = "steps", plugin = "pretty", tags = "@Web")
 
 public class runTest extends AbstractTestNGCucumberTests {
-	
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
 }
